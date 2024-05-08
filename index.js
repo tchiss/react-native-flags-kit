9adcce0775d292c3803e4b65471c360815e88745
Nestor TCHISSAMBOT <nestor.tchissambot@royalcanin.com>
2024-05-08 15:19:11 +0200

    refactor(#25): Replace defaultProps with default function parameters in components

--- index.js
+++ index.js
@@ -3,26 +3,23 @@ import PropTypes from 'prop-types';
 import { Image } from 'react-native';
 import * as flags from './src';
 
-const Flag = props => {
-  const flag = flags[props.type][`icons${props.size}`][props.code];
-  const unknownFlag = flags[props.type][`icons${props.size}`]['unknown'];
+const Flag = ({ size = 64, type = "shiny", code, style }) => {
+  const flag = flags[type][`icons${size}`][code];
+  const unknownFlag = flags[type][`icons${size}`]['unknown'];
 
   return (
-    <Image
-      source={flag || unknownFlag}
-      style={[{ width: props.size, height: props.size }, props.style]}
-    />
+      <Image
+          source={flag || unknownFlag}
+          style={[{ width: size, height: size }, style]}
+      />
   );
 };
 
 Flag.propTypes = {
   size: PropTypes.number,
   type: PropTypes.string,
-};
-
-Flag.defaultProps = {
-  size: 64,
-  type: "shiny",
+  code: PropTypes.string.isRequired,
+  style: PropTypes.object,
 };
 
 export default Flag;
